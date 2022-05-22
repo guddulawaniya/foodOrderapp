a40efc635ca28772b6901f876887e5be8f2b6588
guddu <guddulawaniya123@gmail.com>
2022-05-22 08:51:59 +0530

    first commit

--- app/src/main/java/com/example/foodorder/fooddetailActivity.java
+++ app/src/main/java/com/example/foodorder/fooddetailActivity.java
@@ -1,8 +1,12 @@
 package com.example.foodorder;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.ActivityChooserView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class fooddetailActivity extends AppCompatActivity {
 
@@ -10,5 +14,15 @@ public class fooddetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fooddetail);
+        ImageView back = findViewById(R.id.backdetailactivity);
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(fooddetailActivity.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
